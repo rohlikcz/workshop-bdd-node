6f032133b6b7e49ae7c5e76075246371c446aad5
Juan G. Rodríguez <juan-gabriel.rodriguez@sezamo.es>
2022-06-13 11:27:15 +0200

    refactoring (step2)

--- src/entity/Cart.ts
+++ src/entity/Cart.ts
@@ -1,6 +1,7 @@
 import { Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm"
 import CartLine from "./CartLine"
 import Discount from "./Discount"
+import Product from "./Product"
 
 @Entity("carts")
 class Cart {
@@ -23,6 +24,51 @@ class Cart {
         }
     })
     discounts: Discount[]
+
+    public addLine(product: Product, quantity: number): void {
+        if (quantity < 0) {
+            throw "Negative quantity not allowed"
+        }
+
+        const cartLine: CartLine|undefined = this.lines.find((cartLine: CartLine) => cartLine.product.sku == product.sku)
+
+        if (!cartLine) {
+            if (quantity > 0) {
+                const newCartLine: CartLine = new CartLine()
+                newCartLine.product = product
+                newCartLine.cart = this
+                newCartLine.quantity = quantity
+                this.lines.push(newCartLine)
+            }
+        } else {
+            if (quantity == 0) {
+                this.lines.splice(this.lines.indexOf(cartLine), 1)
+            } else {
+                cartLine.quantity = quantity
+            }
+        }
+    }
+
+    private totalLinesPrice(): number {
+        return this.lines
+            .map((cartLine: CartLine) => cartLine.quantity * cartLine.product.price)
+            .reduce((carry: number, current: number) => carry + current, 0)
+    }
+
+    private totalDiscountsPrice(): number {
+        return this.discounts
+            .map((discount: Discount) => discount.value)
+            .reduce((carry: number, current: number) => carry + current, 0)
+    }
+
+    public totalPrice(): number {
+        return Math.round((this.totalLinesPrice() - this.totalDiscountsPrice()) * 100) / 100
+    }
+
+    public quantityOfProduct(sku: string): number {
+        const cartLine: CartLine|undefined = this.lines.find((cartLine: CartLine) => cartLine.product.sku == sku)
+        return cartLine ? cartLine.quantity : 0
+    }
 }
 
 export default Cart
